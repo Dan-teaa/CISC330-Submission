9588adc312f27186be160253f16f2e6e54993b27
Dante Piedalue <132118768+Dan-teaa@users.noreply.github.com>
2024-09-25 16:22:45 -0400

    Checked that the media query from HW2 is working, and added files for in-class-4.

---
+++ in-class-4/in-class-4.js
@@ -0,0 +1,18 @@
+console.log("in-class-4.js Loaded!")
+
+const myNumbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < myNumbers.length; i++) {
+    if (myNumbers[i] % 2 === 0) {
+        // Template Literal ${}
+        console.log(`${myNumbers[i]} is even!`);
+    } else {
+        // Template Literal ${}
+        console.log(`${myNumbers[i]} is odd!`);
+    }
+}
+
+
+
+// Template literals confuse me, but i'll figure it out haha! 
+// Aparently it's this thing -> `
